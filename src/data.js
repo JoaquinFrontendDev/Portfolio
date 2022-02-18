241f58e4060ddba2bd21124479e8e159fb6deccc
Joaquin Retola <sintaxis.developer@gmail.com>
2022-02-18 14:24:04 -0300

    works list fixes

--- src/data.js
+++ src/data.js
@@ -1,14 +1,6 @@
 export const products = [
 	{
 		id: 1,
-		title: 'Meeting Point',
-		description: `Boiler plate website made for my father, he has a camping and needed a page. Funny thing, it was my first ever deployed code, did it when I hade just 1 month studying, knew some HTML, CSS a couple of JavaScript code lines. Watching it now, I definitely improved my desings; either way it will always be my first "Hello World" and that's why it's in my portfolio`,
-		img: [require('./assets/images/meeting-point.jpg')],
-		link: 'https://meeting-point.netlify.app',
-		github: 'https://github.com/SintaxisDev/Meeting-Point',
-	},
-	{
-		id: 2,
 		title: 'Task Time App',
 		description: `Todo-List made on React Js (first project with that tech), wanted to test my skills and know where I can upgrade my knowledge. It's styled with pure CSS, a CRUD that has no Back-End but allows you to temporarily save your tasks in Local Storage. Either way if you delete your task o tasks by mistake, the re-do button with do the trick. Of course has lots of weak points but it's a step up from "Meeting Point"`,
 		img: [require('./assets/images/todolist.jpg')],
@@ -16,7 +8,7 @@ export const products = [
 		github: 'https://github.com/SintaxisDev/Todo-List-App',
 	},
 	{
-		id: 3,
+		id: 2,
 		title: 'Cooky App',
 		description: `This SPA consumes EDAMAM API, most precisely the 'Recipe Search API', and renders a highly atomic part of each recipe (label, image, ingredients, calories). 
 		Recipes also can be filtered by main ingredient and dish type. The API itself has some limitations regarding it's filters, and was a nice challenge for me to use it. 
@@ -26,7 +18,7 @@ export const products = [
 		github: 'https://github.com/SintaxisDev/Cooky-Recipe-App',
 	},
 	{
-		id: 4,
+		id: 3,
 		title: 'Weather App',
 		description: `Pretty straight foward SPA, made in a day maybe two, super simple, it does what it needs to do. No challenges on this one, used OpenWeatherMap, easy to use, very handy API. This quick project was one of my 3/4 fundamentals to learn React, and wanted to make it before I start learning React Native. Maybe in the future some tweaks will be done, but the main idea wasn't anything fancy but pragmatic`,
 		img: [require('./assets/images/weatherapp.jpg')],
